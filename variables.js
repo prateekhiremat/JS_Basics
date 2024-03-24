c1d7c97bce24a09b86f3c699e83d21fdc519222d
prateekhiremath <prateek.s.hiremath123@gmail.com>
2024-03-24 20:15:17 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,44 @@
+//var - redeclared, updated, globalscope
+var a;
+var $gdh;
+var gfgf_hff;
+
+// ~ intialize
+a=10;
+console.log(a)
+
+// ~ redeclare
+var a = 55;
+
+// ~ re-intialize
+a=45
+console.log(a)
+
+
+//let - cannot be redeclared, can be updated/reinitialized, blockscope
+let b;
+
+// ~ intialize
+b=23;
+console.log(b);
+
+// ! re-declare
+// let b;
+
+// ~ re-intialize
+b=56;
+console.log(b);
+
+// ~ declare and intialize
+let c=89;
+
+//const - cannot be redeclared/updated, initialized at the time of declaration, blockscope
+// ! declare
+//const a;
+// ! intialize
+// ! redeclare
+// ! re-intialize
+
+// ~ declare and intialize;
+const x=25;
+console.log(a);
